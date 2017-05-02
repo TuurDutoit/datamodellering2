60eff50644c4624e790aa4c0516f50332cf56b42
Tuur Dutoit <me@tuurdutoit.be>
2017-05-02 12:20:24 +0200

    Let there be a database

--- create.sql
+++ create.sql
@@ -1,7 +1,90 @@
-CREATE TABLE medewerkers (
+CREATE TABLE medewerker (
     id SERIAL PRIMARY KEY,
     naam varchar(20) NOT NULL,
     email varchar(30),
     telefoon varchar(15),
     adres varchar(50)
-)
+);
+
+CREATE TABLE uren (
+    id SERIAL PRIMARY KEY,
+    van TIMESTAMP NOT NULL,
+    tot TIMESTAMP NOT NULL
+);
+
+CREATE TABLE verlof (
+    id SERIAL PRIMARY KEY,
+    van TIMESTAMP NOT NULL,
+    tot TIMESTAMP NOT NULL,
+    betaald BOOLEAN NOT NULL,
+    goedgekeurd BOOLEAN DEFAULT false
+);
+
+CREATE TABLE contract (
+    van TIMESTAMP NOT NULL,
+    tot TIMESTAMP NOT NULL,
+    uurloon smallint NOT NULL
+);
+
+CREATE TABLE contract_type (
+    naam varchar(20) NOT NULL
+);
+
+CREATE TABLE locatie (
+    naam varchar(20) NOT NULL
+);
+
+CREATE TABLE service_area (
+    naam varchar(20) NOT NULL
+);
+
+CREATE TABLE taak (
+    gedaan BOOLEAN NOT NULL,
+    naam varchar(20) NOT NULL,
+    beschrijving text DEFAULT ''
+);
+
+CREATE TABLE fase (
+    naam varchar(20) NOT NULL,
+    beschrijving text DEFAULT ''
+);
+
+CREATE TABLE fase_type (
+    naam varchar(20) NOT NULL,
+    beschrijving text DEFAULT ''
+);
+
+CREATE TABLE project (
+    naam varchar(20) NOT NULL,
+    beschrijving text DEFAULT ''
+);
+
+CREATE TABLE project (
+    naam varchar(20) NOT NULL,
+    beschrijving text DEFAULT '',
+    prijs INT,
+    intern BOOLEAN DEFAULT false
+);
+
+CREATE TABLE rating (
+    score INT,
+    comment TEXT,
+    datum TIMESTAMP
+);
+
+CREATE TABLE klant (
+    id SERIAL PRIMARY KEY,
+    naam varchar(20) NOT NULL,
+    email varchar(30),
+    telefoon varchar(15),
+    adres varchar(50)
+);
+
+CREATE TABLE verbruik (
+    aantal INT NOT NULL
+);
+
+CREATE TABLE materiaal (
+    naam varchar(20) NOT NULL,
+    prijs INT NOT NULL
+);
